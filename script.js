03f88c281e3298db63c486c07dfeb7b4d05d25e6
kanchanr2208 <267316562+kanchanr2208@users.noreply.github.com>
2026-06-19 10:25:04 +0530

    Add default grid, random color, darkening features Put the grid creating code into a function Call it as a default 16*16 grid, and also when the input is prompted Add randomized coloring to the grid pixels Add darkening effect where the color darkens each each "hover" Add timeout to the reset button to reset grid resets before input prompt

--- script.js
+++ script.js
@@ -1,34 +1,51 @@
 let container = document.querySelector(".container");
+let gridSize = 16
+
+
+function createGrid(boxSize) {
+    let numberOfBoxes = boxSize * boxSize;
+
+    for (let i = 1; i <= numberOfBoxes; i++) {
+        let pixel = document.createElement("div");
+        let dimension = 900/boxSize;
+        pixel.style.width = (`${dimension}px`);
+        pixel.style.height = (`${dimension}px`)
+        pixel.classList.add("pixel");
+        container.appendChild(pixel);
+
+        let red = Math.floor(Math.random()*255);
+        let green = Math.floor(Math.random()*255);
+        let blue = Math.floor(Math.random()*255);
+        let opacity = 0.1;
+
+        pixel.addEventListener("mouseenter", e => {
+            e.target.style.backgroundColor = `rgba(${red}, ${green}, ${blue}, ${opacity})`;
+            opacity = opacity + 0.1;
+        })
+    }
+
+}
+
+createGrid(gridSize);
 
 let resetButton = document.querySelector(".reset-button");
 resetButton.addEventListener("click" , e => {
     e.preventDefault();
     container.replaceChildren();
-    let gridSizeInput = prompt("please enter the grid size. For example: for a 16*16 grid, enter 16.");
-    let gridSize = parseInt(gridSizeInput);
-    if (isNaN(gridSize)) {
-        alert("Got something funky. Try again.")
-    } else if (gridSize >= 101) {
-        alert("grid size too high. Please enter a between 5 and 100")
-    } else if (gridSize <= 3) {
-        alert("grid size to small. please enter a between 5 and 100")
-    } else if (gridSizeInput.includes(".") || gridSizeInput.includes("-")){
-        alert("dont try to be sneaky. Try again")
-    } else {
-        let numberOfBoxes = gridSize * gridSize;
-        
-        for (let i = 1; i <= numberOfBoxes; i++) {
-            let pixel = document.createElement("div");
-            let dimension = 900/gridSize;
-            pixel.style.width = (`${dimension}px`);
-            pixel.style.height = (`${dimension}px`)
-            pixel.classList.add("pixel");
-            container.appendChild(pixel);
 
-            pixel.addEventListener("mouseenter", e => {
-                e.target.style.backgroundColor = "orange";
-            })
+    setTimeout(() => {
+        let gridSizeInput = prompt("please enter the grid size. For example: for a 16*16 grid, enter 16.");
+        let gridSize = parseInt(gridSizeInput);
+        if (isNaN(gridSize)) {
+            alert("Got something funky. Try again.")
+        } else if (gridSize >= 101) {
+            alert("grid size too high. Please enter a between 5 and 100")
+        } else if (gridSize <= 3) {
+            alert("grid size to small. please enter a between 5 and 100")
+        } else if (gridSizeInput.includes(".") || gridSizeInput.includes("-")){
+            alert("dont try to be sneaky. Try again")
+        } else {
+            createGrid(gridSize);     
         }
-         
-    }
+    }, 10);  
 })
